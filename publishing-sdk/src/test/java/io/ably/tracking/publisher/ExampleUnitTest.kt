2ebc6e5a59aaf9a53a719e1d451f160363e95f23
Quintin Willison <q@qwuk.net>
2020-11-17 11:03:06 +0000

    Add Gradle module for Android library for the publishing SDK, with some tweaks including addition of TODO comments.

---
+++ publishing-sdk/src/test/java/io/ably/tracking/publisher/ExampleUnitTest.kt
@@ -0,0 +1,19 @@
+package io.ably.tracking.publisher
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ *
+ * TODO replace or remove
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
