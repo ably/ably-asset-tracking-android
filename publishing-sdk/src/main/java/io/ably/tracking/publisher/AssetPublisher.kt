a0b747b453f512ae7666566bd2a6821ae81424ed
Quintin Willison <q@qwuk.net>
2020-11-17 14:01:11 +0000

    Add Java app project, as interop smoke test, plus annotation to Kotlin interface to make it work.

--- publishing-sdk/src/main/java/io/ably/tracking/publisher/AssetPublisher.kt
+++ publishing-sdk/src/main/java/io/ably/tracking/publisher/AssetPublisher.kt
@@ -5,10 +5,11 @@ interface AssetPublisher {
         /**
          * Returns the default builder of Publisher instances.
          */
+        @JvmStatic
         fun publishers(): PublisherBuilder {
             // TODO ensure this can be called from Java - may need @JvmStatic annotation
             // https://kotlinlang.org/docs/tutorials/kotlin-for-py/objects-and-companion-objects.html#companion-objects
-            TODO("To be implemented.")
+            TODO()
         }
     }
 
