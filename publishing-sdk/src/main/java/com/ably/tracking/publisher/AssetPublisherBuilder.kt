616411d42d845eaedec36b3f81c141bf1103e1d4
Kacper Kluka <k.kluka@nomtek.com>
2020-11-18 20:50:29 +0100

    Create interfaces for Asset Publisher SDK

---
+++ publishing-sdk/src/main/java/com/ably/tracking/publisher/AssetPublisherBuilder.kt
@@ -0,0 +1,63 @@
+package com.ably.tracking.publisher
+
+import android.content.Context
+
+
+internal data class AssetPublisherBuilder(
+    val ablyConfiguration: AblyConfiguration? = null,
+    val mapConfiguration: MapConfiguration? = null,
+    val logConfiguration: LogConfiguration? = null,
+    val batteryConfiguration: BatteryConfiguration? = null,
+    val assetMetadataJson: String? = null,
+    val tripMetadataJson: String? = null,
+    val locationUpdatedListener: LocationUpdatedListener? = null,
+    val androidContext: Context? = null,
+    val trackingId: String? = null,
+    val destination: String? = null,
+    val vehicleType: String? = null,
+) : AssetPublisher.Builder {
+    override fun ablyConfig(configuration: AblyConfiguration): AssetPublisher.Builder {
+        TODO("Not yet implemented")
+    }
+
+    override fun mapConfig(configuration: MapConfiguration): AssetPublisher.Builder {
+        TODO("Not yet implemented")
+    }
+
+    override fun logConfig(configuration: LogConfiguration): AssetPublisher.Builder {
+        TODO("Not yet implemented")
+    }
+
+    override fun batteryConfig(configuration: BatteryConfiguration): AssetPublisher.Builder {
+        TODO("Not yet implemented")
+    }
+
+    override fun assetMetadataJson(metadataJsonString: String): AssetPublisher.Builder {
+        TODO("Not yet implemented")
+    }
+
+    override fun tripMetadataJson(metadataJsonString: String): AssetPublisher.Builder {
+        TODO("Not yet implemented")
+    }
+
+    override fun locationUpdatedListener(listener: LocationUpdatedListener): AssetPublisher.Builder {
+        TODO("Not yet implemented")
+    }
+
+    override fun androidContext(context: Context): AssetPublisher.Builder {
+        TODO("Not yet implemented")
+    }
+
+    override fun delivery(
+        trackingId: String,
+        destination: String,
+        vehicleType: String
+    ): AssetPublisher.Builder {
+        TODO("Not yet implemented")
+    }
+
+    override fun start(): AssetPublisher {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ publishing-sdk/src/main/java/com/ably/tracking/publisher/Exceptions.kt
@@ -0,0 +1,4 @@
+package com.ably.tracking.publisher
+
+class BuilderConfigurationIncompleteException :
+    Exception("Some of the required builder parameters are missing")
