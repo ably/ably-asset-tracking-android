89740cefaa1ee60233fb053c950229b8b74ad6ae
Kacper Kluka <k.kluka@nomtek.com>
2020-11-19 11:59:16 +0100

    Add comments to tests according to the "given, when, then" approach

--- publishing-sdk/src/test/java/com/ably/tracking/publisher/FactoryUnitTests.kt
+++ publishing-sdk/src/test/java/com/ably/tracking/publisher/FactoryUnitTests.kt
@@ -11,142 +11,196 @@ import org.junit.Test
 class FactoryUnitTests {
     @Test
     fun `publishers should return AssetPublisher Builder object`() {
+        // given
+        // when
         val builder = AssetPublisher.publishers()
+        // then
         Assert.assertTrue(builder is AssetPublisher.Builder)
     }
 
     @Test
     fun `setting Ably config updates builder field`() {
+        // given
         val configuration = AblyConfiguration("")
+        // when
         val builder = AssetPublisher.publishers().ablyConfig(configuration) as AssetPublisherBuilder
+        // then
         Assert.assertEquals(configuration, builder.ablyConfiguration)
     }
 
     @Test
     fun `setting Ably config returns a new copy of builder`() {
+        // given
         val configuration = AblyConfiguration("")
         val originalBuilder = AssetPublisher.publishers()
+        // when
         val newBuilder = originalBuilder.ablyConfig(configuration)
+        // then
         Assert.assertNotEquals(newBuilder, originalBuilder)
     }
 
     @Test
     fun `setting map config updates builder field`() {
+        // given
         val configuration = MapConfiguration("")
+        // when
         val builder = AssetPublisher.publishers().mapConfig(configuration) as AssetPublisherBuilder
+        // then
         Assert.assertEquals(configuration, builder.mapConfiguration)
     }
 
     @Test
     fun `setting map config returns a new copy of builder`() {
+        // given
         val configuration = MapConfiguration("")
         val originalBuilder = AssetPublisher.publishers()
+        // when
         val newBuilder = originalBuilder.mapConfig(configuration)
+        // then
         Assert.assertNotEquals(newBuilder, originalBuilder)
     }
 
     @Test
     fun `setting logging config updates builder field`() {
+        // given
         val configuration = LogConfiguration(true)
+        // when
         val builder = AssetPublisher.publishers().logConfig(configuration) as AssetPublisherBuilder
+        // then
         Assert.assertEquals(configuration, builder.logConfiguration)
     }
 
     @Test
     fun `setting logging config returns a new copy of builder`() {
+        // given
         val configuration = LogConfiguration(true)
         val originalBuilder = AssetPublisher.publishers()
+        // when
         val newBuilder = originalBuilder.logConfig(configuration)
+        // then
         Assert.assertNotEquals(newBuilder, originalBuilder)
     }
 
     @Test
     fun `setting battery config updates builder field`() {
+        // given
         val configuration = BatteryConfiguration("")
+        // when
         val builder =
             AssetPublisher.publishers().batteryConfig(configuration) as AssetPublisherBuilder
+        // then
         Assert.assertEquals(configuration, builder.batteryConfiguration)
     }
 
     @Test
     fun `setting battery config returns a new copy of builder`() {
+        // given
         val configuration = BatteryConfiguration("")
         val originalBuilder = AssetPublisher.publishers()
+        // when
         val newBuilder = originalBuilder.batteryConfig(configuration)
+        // then
         Assert.assertNotEquals(newBuilder, originalBuilder)
     }
 
     @Test
     fun `setting asset metadata JSON updates builder field`() {
+        // given
         val metadata = ""
+        // when
         val builder =
             AssetPublisher.publishers().assetMetadataJson(metadata) as AssetPublisherBuilder
+        // then
         Assert.assertEquals(metadata, builder.assetMetadataJson)
     }
 
     @Test
     fun `setting asset metadata JSON returns a new copy of builder`() {
+        // given
         val metadata = ""
         val originalBuilder = AssetPublisher.publishers()
+        // when
         val newBuilder = originalBuilder.assetMetadataJson(metadata)
+        // then
         Assert.assertNotEquals(newBuilder, originalBuilder)
     }
 
     @Test
     fun `setting trip metadata JSON updates builder field`() {
+        // given
         val metadata = ""
+        // when
         val builder =
             AssetPublisher.publishers().tripMetadataJson(metadata) as AssetPublisherBuilder
+        // then
         Assert.assertEquals(metadata, builder.tripMetadataJson)
     }
 
     @Test
     fun `setting trip metadata JSON returns a new copy of builder`() {
+        // given
         val metadata = ""
         val originalBuilder = AssetPublisher.publishers()
+        // when
         val newBuilder = originalBuilder.tripMetadataJson(metadata)
+        // then
         Assert.assertNotEquals(newBuilder, originalBuilder)
     }
 
     @Test
     fun `setting location updated listener updates builder field`() {
+        // given
         val listener: LocationUpdatedListener = {}
+        // when
         val builder =
             AssetPublisher.publishers().locationUpdatedListener(listener) as AssetPublisherBuilder
+        // then
         Assert.assertEquals(listener, builder.locationUpdatedListener)
     }
 
     @Test
     fun `setting location updated listener returns a new copy of builder`() {
+        // given
         val listener: LocationUpdatedListener = {}
         val originalBuilder = AssetPublisher.publishers()
+        // when
         val newBuilder = originalBuilder.locationUpdatedListener(listener)
+        // then
         Assert.assertNotEquals(newBuilder, originalBuilder)
     }
 
     @Test
     fun `setting android context updates builder field`() {
+        // given
         val mockedContext = mockk<Context>()
+        // when
         val builder =
             AssetPublisher.publishers().androidContext(mockedContext) as AssetPublisherBuilder
+        // then
         Assert.assertEquals(mockedContext, builder.androidContext)
     }
 
     @Test
     fun `setting android context returns a new copy of builder`() {
+        // given
         val mockedContext = mockk<Context>()
         val originalBuilder = AssetPublisher.publishers()
+        // when
         val newBuilder = originalBuilder.androidContext(mockedContext)
+        // then
         Assert.assertNotEquals(newBuilder, originalBuilder)
     }
 
     @Test
     fun `setting delivery data should update builder fields`() {
+        // given
         val trackingId = "abc"
         val destination = "London"
         val vehicleType = "car"
+        // when
         val builder = AssetPublisher.publishers()
             .delivery(trackingId, destination, vehicleType) as AssetPublisherBuilder
+        // then
         Assert.assertEquals(trackingId, builder.trackingId)
         Assert.assertEquals(destination, builder.destination)
         Assert.assertEquals(vehicleType, builder.vehicleType)
@@ -154,19 +208,24 @@ class FactoryUnitTests {
 
     @Test
     fun `setting delivery data returns a new copy of builder`() {
+        // given
         val trackingId = "abc"
         val destination = "London"
         val vehicleType = "car"
         val originalBuilder = AssetPublisher.publishers()
+        // when
         val newBuilder = originalBuilder.delivery(trackingId, destination, vehicleType)
+        // then
         Assert.assertNotEquals(newBuilder, originalBuilder)
     }
 
     @Test
     fun `setting all data should update all builder fields`() {
+        // given
         val builder = AssetPublisher.publishers()
         assertAllBuilderFieldsAreNull(builder as AssetPublisherBuilder)
         val mockedContext = mockk<Context>()
+        // when
         val updatedBuilder = builder
             .ablyConfig(AblyConfiguration(""))
             .mapConfig(MapConfiguration(""))
@@ -177,6 +236,7 @@ class FactoryUnitTests {
             .locationUpdatedListener { }
             .androidContext(mockedContext)
             .delivery("", "", "")
+        // then
         assertAllBuilderFieldsAreNotNull(updatedBuilder as AssetPublisherBuilder)
     }
 
