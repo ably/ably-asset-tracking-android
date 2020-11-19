01148271a3b264f883d204b19a5fb50fa53c26c8
Quintin Willison <q@qwuk.net>
2020-11-19 16:10:10 +0000

    Fix ktlint contraventions that could be handled automatically by the formatter.

--- publishing-example-app/src/main/java/com/ably/tracking/example/publisher/MainActivity.kt
+++ publishing-example-app/src/main/java/com/ably/tracking/example/publisher/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.ably.tracking.example.publisher
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
