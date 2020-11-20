fc3a93ac2c82abdea9202f4e871b0230fb323024
Quintin Willison <q@qwuk.net>
2020-11-20 11:35:57 +0000

    Ensure all projects have an example Timber call in order to validate the dependency.

--- publishing-example-java-app/src/main/java/com/ably/tracking/example/javapublisher/MainActivity.java
+++ publishing-example-java-app/src/main/java/com/ably/tracking/example/javapublisher/MainActivity.java
@@ -3,12 +3,14 @@ package com.ably.tracking.example.javapublisher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Timber.d("Hello via Timber");
         setContentView(R.layout.activity_main);
     }
 }
