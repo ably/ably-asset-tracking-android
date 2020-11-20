f8ab1d1737eb42ec4373ad4a2013a112741c0fbd
Kacper Kluka <k.kluka@nomtek.com>
2020-11-20 18:15:04 +0100

    Add starting and stopping location updates to DefaultAssetPublisher

--- publishing-sdk/src/main/java/com/ably/tracking/publisher/DefaultAssetPublisher.kt
+++ publishing-sdk/src/main/java/com/ably/tracking/publisher/DefaultAssetPublisher.kt
@@ -1,18 +1,33 @@
 package com.ably.tracking.publisher
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.annotation.SuppressLint
 import android.content.Context
 import android.location.Location
+import android.os.Looper
 import android.util.Log
+import androidx.annotation.RequiresPermission
 import com.google.gson.Gson
+import com.mapbox.android.core.location.LocationEngineCallback
+import com.mapbox.android.core.location.LocationEngineRequest
+import com.mapbox.android.core.location.LocationEngineResult
 import com.mapbox.navigation.core.MapboxNavigation
 import com.mapbox.navigation.core.trip.session.LocationObserver
 import io.ably.lib.realtime.AblyRealtime
 import io.ably.lib.realtime.Channel
+import io.ably.lib.realtime.CompletionListener
+import io.ably.lib.types.AblyException
+import io.ably.lib.types.ErrorInfo
+
+private const val DEFAULT_INTERVAL_IN_MILLISECONDS = 500L
+private const val DEFAULT_MAX_WAIT_TIME = DEFAULT_INTERVAL_IN_MILLISECONDS * 10
 
 @SuppressLint("LogConditional", "LogNotTimber")
-internal class DefaultAssetPublisher(
-    ablyConfiguration: AblyConfiguration,
+internal class DefaultAssetPublisher
+@RequiresPermission(anyOf = [ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION])
+constructor(
+    private val ablyConfiguration: AblyConfiguration,
     mapConfiguration: MapConfiguration,
     trackingId: String,
     private val locationUpdatedListener: LocationUpdatedListener,
@@ -24,6 +39,16 @@ internal class DefaultAssetPublisher(
     private val mapboxNavigation: MapboxNavigation
     private val ably: AblyRealtime
     private val channel: Channel
+    private val locationEngingeCallback = object : LocationEngineCallback<LocationEngineResult> {
+        override fun onSuccess(result: LocationEngineResult?) {
+            Log.w("TestLocation", result!!.lastLocation!!.latitude.toString())
+        }
+
+        override fun onFailure(exception: java.lang.Exception) {
+            Log.e("MapboxException", exception.toString())
+        }
+    }
+    private var isTracking: Boolean = false
 
     init {
         ably = AblyRealtime(ablyConfiguration.apiKey)
@@ -36,6 +61,7 @@ internal class DefaultAssetPublisher(
                 .build()
         )
         setupLocationUpdatesListener()
+        startLocationUpdates()
     }
 
     private fun setupLocationUpdatesListener() {
@@ -70,7 +96,76 @@ internal class DefaultAssetPublisher(
         locationUpdatedListener(enhancedLocation)
     }
 
+    @RequiresPermission(anyOf = [ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION])
+    @Synchronized
+    private fun startLocationUpdates() {
+        if (!isTracking) {
+            isTracking = true
+
+            try {
+                channel.presence.enterClient(
+                    ablyConfiguration.clientId, "",
+                    object : CompletionListener {
+                        override fun onSuccess() = Unit
+
+                        override fun onError(reason: ErrorInfo?) {
+                            // TODO - handle error
+                            Log.e(TAG, "Unable to enter presence")
+                        }
+                    }
+                )
+            } catch (e: AblyException) {
+                // TODO - handle exception
+                e.printStackTrace()
+            }
+
+            Log.e(TAG, "startLocationUpdates")
+
+            mapboxNavigation.toggleHistory(true)
+            mapboxNavigation.startTripSession()
+
+            // TODO: this is involves the main thread, needs to be checked for running in the background
+            mapboxNavigation.navigationOptions.locationEngine.requestLocationUpdates(
+                LocationEngineRequest.Builder(DEFAULT_INTERVAL_IN_MILLISECONDS)
+                    .setPriority(LocationEngineRequest.PRIORITY_NO_POWER)
+                    .setMaxWaitTime(DEFAULT_MAX_WAIT_TIME)
+                    .build(),
+                locationEngingeCallback,
+                Looper.getMainLooper()
+            )
+        }
+    }
+
     override fun stop() {
-        TODO()
+        stopLocationUpdates()
+        ably.close()
+    }
+
+    @Synchronized
+    private fun stopLocationUpdates() {
+        if (isTracking) {
+            try {
+                channel.presence.leaveClient(
+                    ablyConfiguration.clientId, "",
+                    object : CompletionListener {
+                        override fun onSuccess() = Unit
+
+                        override fun onError(reason: ErrorInfo?) {
+                            // TODO - handle error
+                            Log.e(TAG, "Unable to leave presence")
+                        }
+                    }
+                )
+            } catch (e: AblyException) {
+                // TODO - handle exception
+                e.printStackTrace()
+            }
+            isTracking = false
+            mapboxNavigation.navigationOptions.locationEngine.removeLocationUpdates(
+                locationEngingeCallback
+            )
+            mapboxNavigation.toggleHistory(false)
+            mapboxNavigation.toggleHistory(true)
+        }
     }
 }
