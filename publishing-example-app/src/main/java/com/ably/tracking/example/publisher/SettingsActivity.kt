24e2bf1ed26acdc49a2daa0070bb71d83c5e5afc
Kacper Kluka <k.kluka@nomtek.com>
2020-11-25 13:42:38 +0100

    Add basic settings activity

---
+++ publishing-example-app/src/main/java/com/ably/tracking/example/publisher/SettingsActivity.kt
@@ -0,0 +1,22 @@
+package com.ably.tracking.example.publisher
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainer, SettingsFragment())
+            .commit()
+    }
+}
+
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.settings_preferences)
+        // TODO - fetch files with history data from S3
+    }
+}
