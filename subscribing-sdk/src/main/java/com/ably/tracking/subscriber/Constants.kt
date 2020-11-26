1ef635f7c31af87baa0dbd5c2a7f086211432daa
Kacper Kluka <k.kluka@nomtek.com>
2020-11-26 15:36:20 +0100

    Add default AssetSubscriber implementation

---
+++ subscribing-sdk/src/main/java/com/ably/tracking/subscriber/Constants.kt
@@ -0,0 +1,10 @@
+package com.ably.tracking.subscriber
+
+const val MILLISECONDS_PER_SECOND = 1000
+
+const val LOCATION_TYPE_FUSED = "fused"
+
+object EventNames {
+    const val RAW = "raw"
+    const val ENHANCED = "enhanced"
+}
---
+++ subscribing-sdk/src/main/java/com/ably/tracking/subscriber/MessageModels.kt
@@ -0,0 +1,25 @@
+package com.ably.tracking.subscriber
+
+const val GEOMETRY_LONG_INDEX = 0
+const val GEOMETRY_LAT_INDEX = 1
+
+data class GeoJsonMessage(
+    val type: String,
+    val geometry: GeoJsonGeometry,
+    val properties: GeoJsonProperties
+) {
+    // WARNING: Don't add fields to this class because they will be serialized and present in JSON
+
+    fun synopsis(): String =
+        "[time:${properties.time}; lon:${geometry.coordinates[GEOMETRY_LONG_INDEX]} lat:${geometry.coordinates[GEOMETRY_LAT_INDEX]}; brg:${properties.bearing}]"
+}
+
+data class GeoJsonGeometry(val type: String, val coordinates: List<Double>)
+
+data class GeoJsonProperties(
+    val accuracyHorizontal: Float,
+    val altitude: Double,
+    val bearing: Float,
+    val speed: Float,
+    val time: Double
+)
