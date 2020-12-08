99ca2250764e7a9608ff9b5dac51219afaa38904
Quintin Willison <q@qwuk.net>
2020-12-08 18:04:01 +0000

    Add first stab at a proximity based resolution policy implementation.

---
+++ publishing-sdk/src/main/java/com/ably/tracking/publisher/policies/ProximityBasedResolutionPolicyFactory.kt
@@ -0,0 +1,68 @@
+package com.ably.tracking.publisher.policies
+
+import com.ably.tracking.Resolution
+import com.ably.tracking.publisher.Proximity
+import com.ably.tracking.publisher.ResolutionPolicy
+import com.ably.tracking.publisher.ResolutionRequest
+import com.ably.tracking.publisher.Trackable
+
+class ProximityBasedResolutionPolicyFactory(
+    private val preResolution: Resolution,
+    private val postResolution: Resolution,
+    private val threshold: Proximity
+) : ResolutionPolicy.Factory {
+    override fun createResolutionPolicy(
+        hooks: ResolutionPolicy.Hooks,
+        methods: ResolutionPolicy.Methods
+    ): ResolutionPolicy {
+        return Policy(hooks, methods)
+    }
+
+    private inner class Policy(
+        hooks: ResolutionPolicy.Hooks,
+        private val methods: ResolutionPolicy.Methods
+    ) : ResolutionPolicy {
+        private var thresholdReached = false
+
+        init {
+            hooks.trackables(Listener())
+        }
+
+        override fun resolve(requests: Set<ResolutionRequest>): Resolution {
+            // We are ignoring the set of resolution requests, and simply returning
+            // one of two resolutions depending on whether the proximity threshold
+            // has been reached or not.
+            return if (thresholdReached) postResolution else preResolution
+        }
+
+        private inner class Listener : ResolutionPolicy.Hooks.TrackableSetListener {
+            private val handler = Handler()
+
+            override fun onTrackableAdded(trackable: Trackable) {
+                // Implementation intentionally empty. We are not interested in this event.
+            }
+
+            override fun onTrackableRemoved(trackable: Trackable) {
+                // Implementation intentionally empty. We are not interested in this event.
+            }
+
+            override fun onActiveTrackableChanged(trackable: Trackable?) {
+                if (null == trackable) {
+                    methods.cancelProximityThreshold()
+                } else {
+                    methods.setProximityThreshold(threshold, handler)
+                }
+            }
+
+            private inner class Handler : ResolutionPolicy.Methods.ProximityHandler {
+                override fun onProximityReached(threshold: Proximity) {
+                    thresholdReached = true
+                }
+
+                override fun onProximityCancelled() {
+                    // Implementation intentionally empty. We are not interested in this event.
+                }
+            }
+        }
+    }
+}
