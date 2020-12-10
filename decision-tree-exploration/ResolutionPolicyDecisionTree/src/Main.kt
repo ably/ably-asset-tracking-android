0cc7ddd9007e1816ab71015a880bc8e16d22221d
Ivan Kavalerov <ivan.kavalerov@ably.com>
2020-12-10 00:37:01 +0000

    Add proper fallback logic

--- decision-tree-exploration/ResolutionPolicyDecisionTree/src/Main.kt
+++ decision-tree-exploration/ResolutionPolicyDecisionTree/src/Main.kt
@@ -2,6 +2,8 @@ fun main() {
     TODO()
 }
 
+
+
 enum class BatteryThresholdState {
     BELOW,
     ABOVE
@@ -17,6 +19,11 @@ enum class ProximityThresholdState {
     ABOVE
 }
 
+class ResolutionsParametersState(
+    val proximityState: ProximityThresholdState,
+    val subscribersPresenceState: SubscribersPresenceState
+)
+
 class Accuracy
 typealias Proximity = Float
 typealias BatteryLevel = Float
@@ -31,9 +38,10 @@ data class Subscriber (
     var requestedResolution: Resolution?
 )
 
+// How application developer can supply a set of parameters to trackable. This set of parameters, for example, can be obtained from a remote location (i.e. server)
 data class Trackable(
-    val resolutionParameters:
-        HashMap<Triple<BatteryThresholdState, ProximityThresholdState, SubscribersPresenceState>, Resolution>,
+    val defaultResolution: Resolution,
+    val resolutionParameters: HashMap<ResolutionsParametersState, Resolution>,
     val proximityTreshold: Proximity,
     val batteryThreshold: BatteryLevel,
     val batteryMultiplier: Float // multiplier to be applied to interval when battery level is below the threshold
@@ -49,7 +57,7 @@ data class Trackable(
     }
 }
 
-fun lowestResolution(resolutions: List<Resolution?>): Resolution? {
+fun winningResolution(resolutions: List<Resolution>): Resolution {
     TODO()
 }
 
@@ -58,13 +66,8 @@ fun applyBatteryMultiplier(batteryMultiplier: Float, resolution: Resolution): Re
 }
 
 
-
-fun computeResolution(trackable: Trackable, deviceBatteryLevel: BatteryLevel, defaultResolution: Resolution): Resolution {
+fun computeNetworkingResolution(trackable: Trackable, deviceBatteryLevel: BatteryLevel): Resolution {
     // identify the current state
-    val batteryState: BatteryThresholdState = if (deviceBatteryLevel < trackable.batteryThreshold)
-        BatteryThresholdState.BELOW
-    else
-        BatteryThresholdState.ABOVE
 
     val proximityState: ProximityThresholdState = if (trackable.proximityToDestination() < trackable.proximityTreshold)
         ProximityThresholdState.BELOW
@@ -73,37 +76,64 @@ fun computeResolution(trackable: Trackable, deviceBatteryLevel: BatteryLevel, de
 
     val subscribers = trackable.subscribers()
     val subscribersPresenceState = if (subscribers.isEmpty())
-            SubscribersPresenceState.NOT_PRESENT
-        else
-            SubscribersPresenceState.PRESENT
+        SubscribersPresenceState.NOT_PRESENT
+    else
+        SubscribersPresenceState.PRESENT
 
-    val currentState = Triple(batteryState, proximityState, subscribersPresenceState)
+    val batteryState = if (deviceBatteryLevel < trackable.batteryThreshold)
+        BatteryThresholdState.BELOW
+    else
+        BatteryThresholdState.ABOVE
 
-    var intermediateResolution: Resolution? = null
+    val state = ResolutionsParametersState(proximityState, subscribersPresenceState)
 
-    if (subscribersPresenceState == SubscribersPresenceState.NOT_PRESENT) {
-        intermediateResolution = trackable.resolutionParameters[currentState]
-    } else {
-        val requestedResolutions = trackable.subscribers().map { it.requestedResolution }.toMutableList()
-        requestedResolutions.add(trackable.resolutionParameters[currentState])
-        intermediateResolution = lowestResolution(requestedResolutions)
+    var intermediateResolution: Resolution
+
+    // First lets asses proximity, and update `intermediateResolution` with candidate for final resolution
+    if (proximityState == ProximityThresholdState.ABOVE)
+        intermediateResolution =
+            trackable.resolutionParameters[state] ?:
+                trackable.defaultResolution
+    else {
+        intermediateResolution =
+            trackable.resolutionParameters[state] ?:
+                trackable.resolutionParameters[ResolutionsParametersState(ProximityThresholdState.ABOVE, SubscribersPresenceState.NOT_PRESENT)] ?:
+                trackable.defaultResolution
     }
 
-    intermediateResolution = intermediateResolution ?: defaultResolution
+    // If there are subscribers, find the lowest resolutions among the requested ones and the candidate we have identified
+    if (subscribersPresenceState == SubscribersPresenceState.PRESENT) {
+        val requestedResolutions = trackable.subscribers().map { it.requestedResolution }.toMutableList()
+        requestedResolutions.add(intermediateResolution)
+        intermediateResolution = winningResolution(requestedResolutions.filterNotNull()) // as I am adding Resolution above
+    }f
 
+    // Apply low battery multiplier
     return if (batteryState == BatteryThresholdState.BELOW)
         applyBatteryMultiplier(trackable.batteryMultiplier, intermediateResolution)
     else
         intermediateResolution
 }
 
+fun computeLocationSamplingResolution(resolutions: List<Resolution>): Resolution {
+    return winningResolution(resolutions)
+}
+
+
+
 /*
 Assumptions made:
-* user supplies parameters for all states
+* this policy can work even if user supplies at least one resolution (default) per trackable. The logic will still take requests from subscribers into account.
+* beyond the default one user can supply additional resolutions for different states, that will be used if present
+* user provides thresholds for proximity and battery per trackable. Setting those at 0 (or another special state) will effectively disable taking those factors into account
 * some subscribers may not request specific resolution
-* if there is conflicting resolution request from subscribers and proximity threshold resolution supplied by user - lower resolution wins
-* the fallback logic right now is extremely simple - it is always to "defaultResolution", but should be "smarter"
+* if there is conflicting resolution request from subscribers and resolution supplied by user - lower resolution wins
+* The fallback logic is hardcoded, but should be logical in most cases
+* logic around battery level is extremely simple - just a multiplier
+* user cannot supply multiple "tiers" for battery or proximity
+* when battery is low, the multiplier is applied even if subscribers requested better resolution
 
 Assumptions addressed and removed from above:
+* the fallback logic right now is extremely simple - it is always to "defaultResolution", but should be "smarter"
 * right now the logic above assumes that user provides separate Resolution parameters for having one and multiple subscribers, we probably will not need that
  */
