77f36b201a0affce4a0fb953d132c59a3472c5c2
Kacper Kluka <k.kluka@nomtek.com>
2020-12-18 16:01:31 +0100

    Add tests that show how Publisher API looks like in Java

---
+++ publishing-example-java-app/src/test/java/com/ably/tracking/example/javapublisher/PublisherInterfaceUsageExamples.java
@@ -0,0 +1,153 @@
+package com.ably.tracking.example.javapublisher;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.location.Location;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.ably.tracking.Accuracy;
+import com.ably.tracking.ConnectionConfiguration;
+import com.ably.tracking.LogConfiguration;
+import com.ably.tracking.Resolution;
+import com.ably.tracking.publisher.DebugConfiguration;
+import com.ably.tracking.publisher.DefaultProximity;
+import com.ably.tracking.publisher.DefaultResolutionConstraints;
+import com.ably.tracking.publisher.DefaultResolutionSet;
+import com.ably.tracking.publisher.Destination;
+import com.ably.tracking.publisher.LocationSourceRaw;
+import com.ably.tracking.publisher.MapConfiguration;
+import com.ably.tracking.publisher.Publisher;
+import com.ably.tracking.publisher.ResolutionConstraints;
+import com.ably.tracking.publisher.ResolutionPolicy;
+import com.ably.tracking.publisher.Trackable;
+import com.ably.tracking.publisher.TransportationMode;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.Before;
+import org.junit.Test;
+
+import io.ably.lib.realtime.ConnectionStateListener;
+
+import static org.mockito.Mockito.RETURNS_SELF;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
+
+@SuppressLint("MissingPermission")
+public class PublisherInterfaceUsageExamples {
+    Context context;
+    Publisher publisher;
+    Publisher.Builder publisherBuilder;
+    ResolutionPolicy.Factory resolutionPolicyFactory;
+
+    @Before
+    public void beforeEach() {
+        context = mock(Context.class);
+        publisher = mock(Publisher.class);
+        publisherBuilder = mock(Publisher.Builder.class, withSettings().defaultAnswer(RETURNS_SELF));
+        resolutionPolicyFactory = mock(ResolutionPolicy.Factory.class);
+        when(publisherBuilder.start()).thenReturn(publisher);
+    }
+
+    @Test
+    public void publisherBuilderUsageExample() {
+        publisherBuilder
+            .androidContext(context)
+            .connection(new ConnectionConfiguration("API_KEY", "CLIENT_ID"))
+            .debug(new DebugConfiguration(connectionStateChange -> {
+                onConnectionStateChanged(connectionStateChange);
+                return null;
+            },
+                new LocationSourceRaw(""),
+                historyData -> {
+                    onHistoryData(historyData);
+                    return null;
+                }))
+            .locationUpdatedListener(location -> {
+                onLocationUpdated(location);
+                return null;
+            })
+            .log(new LogConfiguration(true))
+            .map(new MapConfiguration("API_KEY"))
+            .resolutionPolicy(resolutionPolicyFactory)
+            .start();
+    }
+
+    @Test
+    public void publisherUsageExample() {
+        Trackable trackable = new Trackable("ID", null, null, null);
+        publisher.track(
+            trackable,
+            () -> {
+                onSuccess();
+                return null;
+            },
+            e -> {
+                onError();
+                return null;
+            }
+        );
+        publisher.add(
+            trackable,
+            () -> {
+                onSuccess();
+                return null;
+            },
+            e -> {
+                onError();
+                return null;
+            }
+        );
+        publisher.remove(trackable,
+            b -> {
+                onSuccess();
+                return null;
+            },
+            e -> {
+                onError();
+                return null;
+            });
+        Trackable activeTrackable = publisher.getActive();
+        TransportationMode transportationMode = new TransportationMode("TBC");
+        publisher.setTransportationMode(transportationMode);
+        transportationMode = publisher.getTransportationMode();
+        publisher.stop();
+    }
+
+    @Test
+    public void trackableCreationExample() {
+        Trackable trackable = new Trackable(
+            "ID",
+            "METADATA",
+            new Destination(1.0, 1.0),
+            new DefaultResolutionConstraints(
+                new DefaultResolutionSet(new Resolution(Accuracy.MAXIMUM, 1L, 1.0)),
+                new DefaultProximity(1L, 1.0),
+                50F,
+                3F
+            )
+        );
+    }
+
+    private void onSuccess() {
+
+    }
+
+    private void onError() {
+
+    }
+
+    private void onConnectionStateChanged(ConnectionStateListener.ConnectionStateChange connectionStateChange) {
+
+    }
+
+    private void onHistoryData(String historyData) {
+
+    }
+
+    private void onLocationUpdated(Location location) {
+
+    }
+}
