731f5bb5a26cdee01e3797a98d6f5e82e51b55b8
Kacper Kluka <k.kluka@nomtek.com>
2020-12-22 21:10:43 +0100

    Use custom Google or Fused Android Location Engine in Default Publisher

--- publishing-sdk/src/main/java/com/ably/tracking/publisher/DefaultPublisher.kt
+++ publishing-sdk/src/main/java/com/ably/tracking/publisher/DefaultPublisher.kt
@@ -18,7 +18,11 @@ import com.ably.tracking.common.getPresenceData
 import com.ably.tracking.common.toGeoJson
 import com.ably.tracking.common.toJsonArray
 import com.ably.tracking.publisher.debug.AblySimulationLocationEngine
+import com.ably.tracking.publisher.locationengine.LocationEngineUtils
+import com.ably.tracking.publisher.locationengine.GoogleLocationEngine
+import com.ably.tracking.publisher.locationengine.FusedAndroidLocationEngine
 import com.google.gson.Gson
+import com.mapbox.android.core.location.LocationEngine
 import com.mapbox.api.directions.v5.models.DirectionsRoute
 import com.mapbox.api.directions.v5.models.RouteOptions
 import com.mapbox.navigation.base.internal.extensions.applyDefaultParams
@@ -110,6 +114,7 @@ constructor(
             context,
             mapConfiguration.apiKey
         )
+        mapboxBuilder.locationEngine(getBestLocationEngine(context))
         debugConfiguration?.locationSource?.let { locationSource ->
             when (locationSource) {
                 is LocationSourceAbly -> {
@@ -129,6 +134,13 @@ constructor(
         mapboxNavigation.registerLocationObserver(locationObserver)
         startLocationUpdates()
     }
+
+    private fun getBestLocationEngine(context: Context): LocationEngine =
+        if (LocationEngineUtils.hasGoogleLocationServices(context)) {
+            GoogleLocationEngine(context)
+        } else {
+            FusedAndroidLocationEngine(context)
+        }
 
     private fun useAblySimulationLocationEngine(
         mapboxBuilder: NavigationOptions.Builder,
