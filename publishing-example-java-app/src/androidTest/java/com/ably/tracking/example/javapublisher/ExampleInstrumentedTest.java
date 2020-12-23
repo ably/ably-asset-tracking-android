58be9c2ec48b85150f98604cdbdef1687fe78131
Quintin Willison <q@qwuk.net>
2020-12-23 16:01:13 +0000

    Fix existing tests, overlooked because we've not been running the connectedCheck task thus far.

--- publishing-example-java-app/src/androidTest/java/com/ably/tracking/example/javapublisher/ExampleInstrumentedTest.java
+++ publishing-example-java-app/src/androidTest/java/com/ably/tracking/example/javapublisher/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("io.ably.tracking.example.javapublisher", appContext.getPackageName());
+        assertEquals("com.ably.tracking.example.javapublisher", appContext.getPackageName());
     }
 }
--- publishing-sdk/src/androidTest/java/com/ably/tracking/publisher/ExampleInstrumentedTest.kt
+++ publishing-sdk/src/androidTest/java/com/ably/tracking/publisher/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("io.ably.tracking.publisher.test", appContext.packageName)
+        assertEquals("com.ably.tracking.publisher.test", appContext.packageName)
     }
 }
