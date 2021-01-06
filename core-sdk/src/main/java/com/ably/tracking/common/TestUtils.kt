5081145b1c39d826661b913ed3bfdf086277ae0b
Quintin Willison <q@qwuk.net>
2021-01-06 16:08:35 +0000

    Add some interface commentary to the test utils.

--- core-sdk/src/main/java/com/ably/tracking/common/TestUtils.kt
+++ core-sdk/src/main/java/com/ably/tracking/common/TestUtils.kt
@@ -3,18 +3,31 @@ package com.ably.tracking.common
 import java.util.concurrent.Semaphore
 import java.util.concurrent.TimeUnit
 
+/**
+ * A timeout appropriate for most integration tests.
+ */
 private const val DEFAULT_ACQUIRE_TIMEOUT_IN_SECONDS = 5L
 
+/**
+ * Encapsulates a semaphore with a single permit, acquired from the outset.
+ */
 class TestLock {
     val semaphore = Semaphore(1).apply { this.acquire() }
 
+    /**
+     * Calls tryAcquire on the encapsulated semaphore.
+     * @throws Exception If the timeout was reached before the permit could be acquired.
+     */
     fun acquire(timeoutInSeconds: Long = DEFAULT_ACQUIRE_TIMEOUT_IN_SECONDS) {
         val hasAcquired = semaphore.tryAcquire(1, timeoutInSeconds, TimeUnit.SECONDS)
         if (!hasAcquired) {
-            throw Exception("TestLock Timeout")
+            throw Exception("Timeout when attempting to acquire semaphore permit.")
         }
     }
 
+    /**
+     * Calls release on the encapsulated semaphore.
+     */
     fun release() {
         semaphore.release()
     }
