653e0a5f6cbe92c8eaed3227a6b927077d9a7f4f
Quintin Willison <q@qwuk.net>
2021-01-11 14:15:31 +0000

    Expand the connection state change interface to include error information.

--- core-sdk/src/main/java/com/ably/tracking/ListenersInterfaces.kt
+++ core-sdk/src/main/java/com/ably/tracking/ListenersInterfaces.kt
@@ -19,11 +19,60 @@ enum class ConnectionState {
     FAILED,
 }
 
-data class ConnectionStateChange(
-    val previousState: ConnectionState,
-    val state: ConnectionState
+/**
+ * Information about an error reported by the Ably service.
+ */
+data class ErrorInformation(
+    /**
+     * Ably specific error code. Defined [here](https://github.com/ably/ably-common/blob/main/protocol/errors.json).
+     */
+    val code: Int,
+
+    /**
+     * Analogous to HTTP status code.
+     */
+    val statusCode: Int,
+
+    /**
+     * An explanation of what went wrong, in a format readable by humans.
+     * Can be written to logs or presented to users, but is not intended to be machine parsed.
+     */
+    val message: String,
+
+    /**
+     * A URL for customers to find more help on the error code.
+     */
+    val href: String?,
+
+    /**
+     * An error underlying this error which caused this failure.
+     */
+    val cause: ErrorInformation?
 )
 
+/**
+ * A change in state of a connection to the Ably service.
+ */
+data class ConnectionStateChange(
+    /**
+     * The new state, which is now current.
+     */
+    val state: ConnectionState,
+
+    /**
+     * The previous state.
+     */
+    val previousState: ConnectionState,
+
+    /**
+     * Information about what went wrong, if [state] is failed or failing in some way.
+     */
+    val errorInformation: ErrorInformation?
+)
+
+/**
+ * Defines the signature of a function accepting state change events relating connectivity to the Ably service.
+ */
 typealias ConnectionStateChangeHandler = (ConnectionStateChange) -> Unit
 
 typealias LocationHistoryHandler = (String) -> Unit
--- core-sdk/src/main/java/com/ably/tracking/common/AblyHelpers.kt
+++ core-sdk/src/main/java/com/ably/tracking/common/AblyHelpers.kt
@@ -16,8 +16,31 @@ fun io.ably.lib.realtime.ConnectionState.toTracking() = when (this) {
 }
 
 /**
+ * Extension converting Ably error info objects to the equivalent [ErrorInformation] API presented to users of the Ably
+ * Asset Tracking SDKs.
+ *
+ * The `requestId` field is yet to be implemented by ably-java, however even once it is available then the chances are
+ * that it'll still not be exposed through to users of the Ably Asset Tracking SDKs in order to keep things simple.
+ */
+fun io.ably.lib.types.ErrorInfo.toTracking() =
+    ErrorInformation(
+        this.code,
+        this.statusCode,
+        this.message,
+        this.href, // may be null
+        null // yet to be implemented by ably-java
+    )
+
+/**
  * Extension converting Ably Realtime connection state change events to the equivalent [ConnectionStateChange] API
  * presented to users of the Ably Asset Tracking SDKs.
+ *
+ * We are intentionally not passing on the `event` or `retryIn` fields. Our position on this may change in future, but
+ * they are omitted for now to keep things simple.
  */
 fun io.ably.lib.realtime.ConnectionStateListener.ConnectionStateChange.toTracking() =
-    ConnectionStateChange(this.previous.toTracking(), this.current.toTracking())
+    ConnectionStateChange(
+        this.previous.toTracking(),
+        this.current.toTracking(),
+        this.reason?.toTracking()
+    )
