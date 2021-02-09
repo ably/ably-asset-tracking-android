c10bba1af4519d12ddc06702b53d1b6291e04466
Kacper Kluka <k.kluka@nomtek.com>
2021-02-09 14:24:59 +0100

    Simplify Ably connection states to: online, offline, failed

--- common/src/main/java/com/ably/tracking/common/AblyHelpers.kt
+++ common/src/main/java/com/ably/tracking/common/AblyHelpers.kt
@@ -11,13 +11,13 @@ import io.ably.lib.types.ClientOptions
  * the Ably Asset Tracking SDKs.
  */
 fun io.ably.lib.realtime.ConnectionState.toTracking() = when (this) {
-    io.ably.lib.realtime.ConnectionState.initialized -> ConnectionState.INITIALIZED
-    io.ably.lib.realtime.ConnectionState.connecting -> ConnectionState.CONNECTING
-    io.ably.lib.realtime.ConnectionState.connected -> ConnectionState.CONNECTED
-    io.ably.lib.realtime.ConnectionState.disconnected -> ConnectionState.DISCONNECTED
-    io.ably.lib.realtime.ConnectionState.suspended -> ConnectionState.SUSPENDED
-    io.ably.lib.realtime.ConnectionState.closing -> ConnectionState.CLOSING
-    io.ably.lib.realtime.ConnectionState.closed -> ConnectionState.CLOSED
+    io.ably.lib.realtime.ConnectionState.initialized -> ConnectionState.OFFLINE
+    io.ably.lib.realtime.ConnectionState.connecting -> ConnectionState.OFFLINE
+    io.ably.lib.realtime.ConnectionState.connected -> ConnectionState.ONLINE
+    io.ably.lib.realtime.ConnectionState.disconnected -> ConnectionState.OFFLINE
+    io.ably.lib.realtime.ConnectionState.suspended -> ConnectionState.OFFLINE
+    io.ably.lib.realtime.ConnectionState.closing -> ConnectionState.OFFLINE
+    io.ably.lib.realtime.ConnectionState.closed -> ConnectionState.OFFLINE
     io.ably.lib.realtime.ConnectionState.failed -> ConnectionState.FAILED
 }
 
