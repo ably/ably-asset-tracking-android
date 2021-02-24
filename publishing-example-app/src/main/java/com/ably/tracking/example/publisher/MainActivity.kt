9778fd9f5de7328edae2c5b230f5d1c480fb0ec0
Kacper Kluka <k.kluka@nomtek.com>
2021-02-24 13:54:20 +0100

    Update trackables list when publishers trackables set changes

--- publishing-example-app/src/main/java/com/ably/tracking/example/publisher/MainActivity.kt
+++ publishing-example-app/src/main/java/com/ably/tracking/example/publisher/MainActivity.kt
@@ -6,6 +6,12 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import pub.devrel.easypermissions.AfterPermissionGranted
 import pub.devrel.easypermissions.EasyPermissions
 import timber.log.Timber
@@ -15,6 +21,11 @@ private const val REQUEST_LOCATION_PERMISSION = 1
 
 class MainActivity : PublisherServiceActivity() {
     private lateinit var appPreferences: AppPreferences
+    private val trackablesAdapter = TrackablesAdapter()
+    private var trackablesUpdateJob: Job? = null
+
+    // SupervisorJob() is used to keep the scope working after any of its children fail
+    private val scope = CoroutineScope(Dispatchers.Main + SupervisorJob())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +46,21 @@ class MainActivity : PublisherServiceActivity() {
 
         trackablesRecyclerView.adapter = trackablesAdapter
         trackablesRecyclerView.layoutManager = LinearLayoutManager(this)
-        trackablesAdapter.onItemClicked = { onTrackableClicked(it.id) }
+        trackablesAdapter.onItemClickedCallback = { onTrackableClicked(it.id) }
+    }
+
+    override fun onPublisherServiceConnected(publisherService: PublisherService) {
+        publisherService.publisher?.let { publisher ->
+            trackablesUpdateJob = publisher.trackables
+                .onEach {
+                    trackablesAdapter.trackables = it.toList()
+                }
+                .launchIn(scope)
+        }
+    }
+
+    override fun onPublisherServiceDisconnected() {
+        trackablesUpdateJob?.cancel()
     }
 
     override fun onStart() {
