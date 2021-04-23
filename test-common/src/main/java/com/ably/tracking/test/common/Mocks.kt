47d8f0379e63ac1ce0fd85b5150ad1e7a6339a28
Kacper Kluka <k.kluka@nomtek.com>
2021-04-23 08:44:43 +0200

    Move location mocks to the common test module

--- test-common/src/main/java/com/ably/tracking/test/common/Mocks.kt
+++ test-common/src/main/java/com/ably/tracking/test/common/Mocks.kt
@@ -1,5 +1,12 @@
 package com.ably.tracking.test.common
 
 import android.location.Location
+import io.mockk.every
+import io.mockk.mockk
 
-fun anyLocation() = Location("fused")
+fun createLocation(lat: Double, lng: Double): Location = mockk<Location>().apply {
+    every { longitude } returns lng
+    every { latitude } returns lat
+}
+
+fun anyLocation() = createLocation(1.0, 1.0)
