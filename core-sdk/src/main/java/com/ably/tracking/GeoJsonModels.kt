8b3b1f3467541d9bc08ec70990a999bedc758bbe
Kacper Kluka <k.kluka@nomtek.com>
2021-04-29 09:03:50 +0200

    Add Dto annotation to GeoJSON classes

--- core-sdk/src/main/java/com/ably/tracking/GeoJsonModels.kt
+++ core-sdk/src/main/java/com/ably/tracking/GeoJsonModels.kt
@@ -1,14 +1,17 @@
 package com.ably.tracking
 
 // WARNING: Don't add fields to this class because they will be serialized and present in JSON
+@Dto
 data class GeoJsonMessage(
     val type: String,
     val geometry: GeoJsonGeometry,
     val properties: GeoJsonProperties
 )
 
+@Dto
 data class GeoJsonGeometry(val type: String, val coordinates: List<Double>)
 
+@Dto
 data class GeoJsonProperties(
     val accuracyHorizontal: Float,
     val bearing: Float,
