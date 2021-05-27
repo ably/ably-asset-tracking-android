da33a9e72f946c91addad967badce8ceceec3f96
Kacper Kluka <k.kluka@nomtek.com>
2021-05-27 11:34:12 +0200

    Add docs to our custom Location type

--- core-sdk/src/main/java/com/ably/tracking/LocationUpdateModels.kt
+++ core-sdk/src/main/java/com/ably/tracking/LocationUpdateModels.kt
@@ -26,15 +26,46 @@ enum class LocationUpdateType {
     PREDICTED, ACTUAL
 }
 
+/**
+ * Represents the geographic location of the publisher.
+ */
 data class Location(
+    /**
+     * Latitude of the location in degrees.
+     */
     val latitude: Double,
+    /**
+     * Longitude of the location in degrees.
+     */
     val longitude: Double,
+    /**
+     * Altitude of the location in meters above the WGS 84 reference ellipsoid.
+     * 0.0 if not available.
+     */
     val altitude: Double,
+    /**
+     * Estimated horizontal accuracy of the location, radial, in meters.
+     * 0.0 if not available.
+     */
     val accuracy: Float,
+    /**
+     * Bearing of the location in degrees.
+     * 0.0 if not available.
+     */
     val bearing: Float,
+    /**
+     * Speed of the location in meters per second.
+     * 0.0 if not available.
+     */
     val speed: Float,
+    /**
+     * Timestamp of the location in milliseconds since the epoch.
+     */
     val time: Long
 ) {
+    /**
+     * Convenience function that maps the [Location] object to Android's [android.location.Location] object.
+     */
     fun toAndroid(): android.location.Location {
         return android.location.Location("fused").apply {
             longitude = this@Location.longitude
