f73876e1a64407fd88480721cad44786f1e42a66
Kacper Kluka <k.kluka@nomtek.com>
2021-06-01 14:50:57 +0200

    Reword log message when unexpected presence data is received

--- common/src/main/java/com/ably/tracking/common/Ably.kt
+++ common/src/main/java/com/ably/tracking/common/Ably.kt
@@ -275,7 +275,7 @@ constructor(
                             if (parsedMessage != null) {
                                 listener(parsedMessage)
                             } else {
-                                Timber.w("Presence message in wrong format: $presenceMessage")
+                                Timber.w("Presence message in unexpected format: $presenceMessage")
                             }
                         }
                     }
@@ -285,7 +285,7 @@ constructor(
                     if (parsedMessage != null) {
                         listener(parsedMessage)
                     } else {
-                        Timber.w("Presence message in wrong format: $it")
+                        Timber.w("Presence message in unexpected format: $it")
                     }
                 }
             } catch (exception: AblyException) {
