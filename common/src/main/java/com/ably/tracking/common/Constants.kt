a72c54c1cac051079205596db03562a24abdacac
Quintin Willison <q@qwuk.net>
2021-06-08 10:54:32 +0100

    Add explanatory commentary to the METADATA_TRIP channel name constant.

--- common/src/main/java/com/ably/tracking/common/Constants.kt
+++ common/src/main/java/com/ably/tracking/common/Constants.kt
@@ -7,6 +7,13 @@ const val MILLISECONDS_PER_MINUTE = MILLISECONDS_PER_SECOND * 60
 const val METERS_PER_KILOMETER = 1000
 
 object ChannelNames {
+    /**
+     * This is a metachannel, which will be defined across the Ably infrastructure.
+     *
+     * **Note**: Simply using `tracking` as the namespace prefix wouldn't be enough as,
+     * in the wider Ably context, `tracking` can mean many things. Whereas,
+     * `asset-tracking` specifies a product.
+     */
     const val METADATA_TRIP = "[meta]asset-tracking:trip-lifecycle"
 }
 
