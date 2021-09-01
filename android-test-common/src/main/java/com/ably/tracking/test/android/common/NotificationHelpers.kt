8d66606295d2265d6bd7788848cec83bae4d4edd
Kacper Kluka <k.kluka@nomtek.com>
2021-09-01 10:24:06 +0200

    Add commentary to the way of creating notification channel

--- android-test-common/src/main/java/com/ably/tracking/test/android/common/NotificationHelpers.kt
+++ android-test-common/src/main/java/com/ably/tracking/test/android/common/NotificationHelpers.kt
@@ -8,6 +8,9 @@ import android.os.Build
 const val NOTIFICATION_CHANNEL_ID = "test-notification-channel"
 
 fun createNotificationChannel(context: Context) {
+    // This guard is required by the official Android documentation as the notification
+    // channels API is not available in the support library.
+    // https://developer.android.com/training/notify-user/channels#CreateChannel
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
         context.getSystemService(NotificationManager::class.java).apply {
             createNotificationChannel(
