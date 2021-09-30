909450ac802e00f9c8053e916e0b88bd4468faa7
Kacper Kluka <k.kluka@nomtek.com>
2021-09-30 08:30:27 +0200

    Use named parameters for hideKeyboard extension

--- publishing-example-app/src/main/java/com/ably/tracking/example/publisher/Extensions.kt
+++ publishing-example-app/src/main/java/com/ably/tracking/example/publisher/Extensions.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
+private const val NO_FLAGS = 0
+
 fun Context.hideKeyboard(view: View) {
     (getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager)?.hideSoftInputFromWindow(
         view.windowToken,
-        0
+        NO_FLAGS
     )
 }
