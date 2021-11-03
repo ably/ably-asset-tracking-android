c89a564983557b86bd8d532802a02d3fd7c8fdf9
ikbalkaya <ceikbal@gmail.com>
2021-11-03 17:57:13 +0000

    Added values from Configuration to modules

--- buildSrc/src/main/kotlin/com/ably/configuration/Configuration.kt
+++ buildSrc/src/main/kotlin/com/ably/configuration/Configuration.kt
@@ -4,9 +4,9 @@ object Configuration {
     const val compileSdk = 31
     const val targetSdk = 30
     const val minSdk = 21
-    const val majorVersion = 4
-    const val minorVersion = 21
-    const val patchVersion = 0
+    const val majorVersion = 1
+    const val minorVersion = 2
+    const val patchVersion = 8
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
     const val artifactGroup = "io.ably"
