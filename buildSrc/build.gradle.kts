d85d84fc0f69cee67df61d23baa7b622896dfa50
ikbalkaya <ceikbal@gmail.com>
2021-11-03 17:21:43 +0000

    Created a buildSrc directory for central script configuration

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,7 @@
+//https://proandroiddev.com/better-dependencies-management-using-buildsrc-kotlin-dsl-eda31cdb81bf
+plugins {
+    `kotlin-dsl`
+}
+repositories {
+    mavenCentral()
+}
---
+++ buildSrc/src/main/kotlin/com/ably/configuration/Configuration.kt
@@ -0,0 +1,13 @@
+package com.ably.configuration
+
+object Configuration {
+    const val compileSdk = 31
+    const val targetSdk = 30
+    const val minSdk = 21
+    const val majorVersion = 4
+    const val minorVersion = 21
+    const val patchVersion = 0
+    const val versionName = "$majorVersion.$minorVersion.$patchVersion"
+    const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
+    const val artifactGroup = "io.ably"
+}
