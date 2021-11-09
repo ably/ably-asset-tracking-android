9569b635edd7308ad46869946cb5d9c3a40218d9
ikbalkaya <ceikbal@gmail.com>
2021-11-09 14:13:53 +0000

    typo / grammar fix

--- publishing-sdk/src/main/java/com/ably/tracking/publisher/guards/TrackableRemovalGuard.kt
+++ publishing-sdk/src/main/java/com/ably/tracking/publisher/guards/TrackableRemovalGuard.kt
@@ -3,7 +3,7 @@ package com.ably.tracking.publisher.guards
 import com.ably.tracking.publisher.Trackable
 
 /**
- * This guard class will keep track or trackables that has been requested for removal. The aim of this class is to
+ * This guard class will keep track of trackables that are requested for removal. The aim of this class is to
  * maintain a set of trackables and expose client functions.
  */
 class TrackableRemovalGuard {
