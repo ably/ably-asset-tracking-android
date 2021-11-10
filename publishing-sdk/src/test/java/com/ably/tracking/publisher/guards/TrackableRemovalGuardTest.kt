65e07c6ec30506daed406e6fa8086bb99962bf52
ikbalkaya <ceikbal@gmail.com>
2021-11-10 16:10:50 +0000

    Fixed existing tests, added new tests

--- publishing-sdk/src/test/java/com/ably/tracking/publisher/guards/TrackableRemovalGuardTest.kt
+++ publishing-sdk/src/test/java/com/ably/tracking/publisher/guards/TrackableRemovalGuardTest.kt
@@ -4,16 +4,19 @@ import com.ably.tracking.publisher.Trackable
 import org.junit.Assert.assertTrue
 import org.junit.Assert.assertFalse
 import org.junit.Test
+import java.lang.Exception
+import com.ably.tracking.common.ResultHandler as ResultHandler1
 
 class TrackableRemovalGuardTest {
-    val trackableRemovalGuard = TrackableRemovalGuard()
+    private val trackableRemovalGuard = TrackableRemovalGuard()
 
     @Test
     fun `marking trackable for removal really marks it for removal`() {
         // given
         val trackable = Trackable("sample")
-        // when
-        trackableRemovalGuard.markForRemoval(trackable)
+        trackableRemovalGuard.markForRemoval(trackable) {
+
+        }
         // then
         assertTrue(trackableRemovalGuard.isMarkedForRemoval(trackable))
     }
@@ -32,10 +35,10 @@ class TrackableRemovalGuardTest {
     fun `removing marked trackable removes it from removal list`() {
         // given
         val trackable = Trackable("sample")
-        trackableRemovalGuard.markForRemoval(trackable)
+        trackableRemovalGuard.markForRemoval(trackable) {}
 
         // when
-        trackableRemovalGuard.removeMarked(trackable)
+        trackableRemovalGuard.removeMarked(trackable, Result.success(true))
 
         // then
         assertFalse(trackableRemovalGuard.isMarkedForRemoval(trackable))
@@ -46,10 +49,10 @@ class TrackableRemovalGuardTest {
         // given
         val trackable = Trackable("sample")
         val otherTrackable = Trackable("sample2")
-        trackableRemovalGuard.markForRemoval(trackable)
+        trackableRemovalGuard.markForRemoval(trackable) {}
 
         // when
-        trackableRemovalGuard.removeMarked(otherTrackable)
+        trackableRemovalGuard.removeMarked(otherTrackable, Result.success(true))
 
         // then
         assertTrue(trackableRemovalGuard.isMarkedForRemoval(trackable))
@@ -60,13 +63,82 @@ class TrackableRemovalGuardTest {
         // given
         val trackable = Trackable("sample")
         val sameTrackable = Trackable("sample")
-        trackableRemovalGuard.markForRemoval(trackable)
-        trackableRemovalGuard.markForRemoval(trackable)
+        trackableRemovalGuard.markForRemoval(trackable) {}
+        trackableRemovalGuard.markForRemoval(trackable) {}
 
         // when
-        trackableRemovalGuard.removeMarked(sameTrackable)
+        trackableRemovalGuard.removeMarked(sameTrackable, Result.success(true))
 
         // then
         assertFalse(trackableRemovalGuard.isMarkedForRemoval(trackable))
     }
+
+    @Test
+    fun `handlers are called after associated trackable was removed`() {
+        // given
+        val trackable = Trackable("sample")
+        var called1 = false
+        var called2 = false
+        trackableRemovalGuard.markForRemoval(trackable) {
+            called1 = true
+        }
+        //add it one more time
+        trackableRemovalGuard.markForRemoval(trackable) {
+            called2 = true
+        }
+
+        // when
+        trackableRemovalGuard.removeMarked(trackable, Result.success(true))
+
+        // then
+        assertTrue(called1 && called2)
+    }
+
+    @Test
+    fun `handlers are not called if associated trackable was not removed`() {
+        // given
+        val trackable = Trackable("sample")
+        var called1 = false
+        var called2 = false
+
+        trackableRemovalGuard.markForRemoval(trackable) {
+            called1 = true
+        }
+        //add it one more time
+        trackableRemovalGuard.markForRemoval(trackable) {
+            called2 = true
+        }
+
+        // when nothing happens
+
+        // then
+        assertFalse(called1 || called2)
+    }
+
+    @Test
+    fun `handler is called with success if removal was successful`() {
+        // given
+        val trackable = Trackable("sample")
+
+        trackableRemovalGuard.markForRemoval(trackable) {
+            //then
+            assertTrue(it.isSuccess)
+        }
+        // when
+        trackableRemovalGuard.removeMarked(trackable, Result.success(true))
+    }
+
+    @Test
+    fun `handler is called with failure if removal was not successful`() {
+        // given
+        val trackable = Trackable("sample")
+
+        trackableRemovalGuard.markForRemoval(trackable) {
+            //then
+            assertTrue(it.isFailure)
+        }
+        // when
+        trackableRemovalGuard.removeMarked(trackable, Result.failure(Exception("simple")))
+    }
+
 }
