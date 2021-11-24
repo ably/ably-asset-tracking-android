5d90762364e4f52b929c18a723f8a121e9db6259
ikbalkaya <ceikbal@gmail.com>
2021-11-24 16:09:10 +0000

    Cleaned up AddTrackableWorker

--- publishing-sdk/src/main/java/com/ably/tracking/publisher/workerqueue/workers/AddTrackableWorker.kt
+++ publishing-sdk/src/main/java/com/ably/tracking/publisher/workerqueue/workers/AddTrackableWorker.kt
@@ -1,6 +1,5 @@
 package com.ably.tracking.publisher.workerqueue.workers
 
-import android.util.Log
 import com.ably.tracking.ConnectionException
 import com.ably.tracking.TrackableState
 import com.ably.tracking.common.Ably
@@ -13,15 +12,12 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
-private const val TAG = "AddTrackableWorker"
-
 internal class AddTrackableWorker(
     private val trackable: Trackable,
-    val callbackFunction: ResultCallbackFunction<StateFlow<TrackableState>>,
+    private val callbackFunction: ResultCallbackFunction<StateFlow<TrackableState>>,
     private val ably: Ably
 ) : Worker {
     override fun doWork(publisherState: DefaultCorePublisher.State): SyncAsyncResult {
-        Log.d(TAG, "doWork: ")
         if (publisherState.trackables.contains(trackable)) {
             return SyncAsyncResult(
                 AddTrackableWorkResult.AlreadyIn(publisherState.trackableStateFlows[trackable.id]!!, callbackFunction),
@@ -30,7 +26,6 @@ internal class AddTrackableWorker(
         }
         return SyncAsyncResult(null, asyncWork = {
             val connectResult = suspendingConnect(publisherState)
-            Log.d(TAG, "doWork: connect result received $connectResult")
             if (connectResult.isSuccess) {
                 AddTrackableWorkResult.Success(trackable, callbackFunction)
             } else {
@@ -50,20 +45,5 @@ internal class AddTrackableWorker(
                 }
             }
         }
-    }
-
-    override fun hashCode(): Int {
-        return trackable.hashCode()
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as AddTrackableWorker
-
-        if (trackable != other.trackable) return false
-
-        return true
     }
 }
