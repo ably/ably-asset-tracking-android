938159fdd62ac6f3b031bbc8ecef46f9a7efec01
ikbalkaya <ceikbal@gmail.com>
2021-11-25 11:32:56 +0000

    Test skeleton for AddTrackableWorker

---
+++ publishing-sdk/src/test/java/com/ably/tracking/publisher/workerqueue/workers/AddTrackableWorkerTest.kt
@@ -0,0 +1,32 @@
+package com.ably.tracking.publisher.workerqueue.workers
+
+import com.ably.tracking.TrackableState
+import com.ably.tracking.common.Ably
+import com.ably.tracking.common.ResultCallbackFunction
+import com.ably.tracking.publisher.DefaultCorePublisher
+import com.ably.tracking.publisher.Trackable
+import io.mockk.mockk
+import kotlinx.coroutines.flow.StateFlow
+
+import org.junit.Before
+import org.junit.Test
+
+class AddTrackableWorkerTest {
+    private lateinit var worker: AddTrackableWorker
+
+    //dependencies
+    //private lateinit var resultCallbackFunction: ResultCallbackFunction<StateFlow<TrackableState>>
+    private val resultCallbackFunction = mockk<ResultCallbackFunction<StateFlow<TrackableState>>>()
+    private val ably = mockk<Ably>(relaxed = true)
+    private val trackable = Trackable("testtrackable")
+    @Before
+    fun setUp() {
+        worker = AddTrackableWorker(trackable,resultCallbackFunction,ably)
+    }
+
+    @Test
+    fun `doWork returns asyncWork when trackable is not added and not being added`() {
+
+    }
+
+}
