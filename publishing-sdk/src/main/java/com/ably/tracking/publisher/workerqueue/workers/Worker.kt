953bb2e54d199aab26f26c724030efd4d9084187
ikbalkaya <ceikbal@gmail.com>
2021-12-01 11:48:37 +0000

    Updated Worker documentation

--- publishing-sdk/src/main/java/com/ably/tracking/publisher/workerqueue/workers/Worker.kt
+++ publishing-sdk/src/main/java/com/ably/tracking/publisher/workerqueue/workers/Worker.kt
@@ -5,15 +5,19 @@ import com.ably.tracking.publisher.workerqueue.SyncAsyncResult
 
 /**
  * A [Worker] interface represents workers which executes synchronous work with an optional synchronous work result
- * and another optional asynchrnous work.
+ * and another optional asynchronous work.
  * */
 internal interface Worker {
     /**
-     * This function is provided in order for implementors  to implement synchronous work, optionally provide a
-     * result for synchronous work and an optional asynchronous work.
-     * @return [SyncAsyncResult] which represent an optional synchronous work result and an optional asynchronous work
-     * [properties] : Current state of publisher to be used by this worker. Please do not modify and access to
-     * this from async work block.
+     * This function is provided in order for implementors  to implement synchronous work. They may  optionally
+     * provide result for the executed synchronous work and an optional asynchronous work the form of a suspending
+     * function.
+     *
+     * @param properties : Current state of publisher to be used by this worker. This state is intended to be used
+     * inside sync work block. Please use this directly only at synchronus part of code. If you need to pass this
+     * into async work lambda, you should copy neccessary properties.
+     *
+     * @return SyncAsyncResult which represents an optional synchronous work result and an optional asynchronous work
      * **/
     fun doWork(properties: DefaultCorePublisher.Properties): SyncAsyncResult
 }
