9b7ea22024c66e2282972e8974111c5803efd1d4
ikbalkaya <ceikbal@gmail.com>
2021-12-01 12:01:40 +0000

    Improved WorkerQueue documentation

--- publishing-sdk/src/main/java/com/ably/tracking/publisher/workerqueue/WorkerQueue.kt
+++ publishing-sdk/src/main/java/com/ably/tracking/publisher/workerqueue/WorkerQueue.kt
@@ -5,13 +5,15 @@ import com.ably.tracking.publisher.workerqueue.workers.Worker
 /**
  * An interface for a  worker queue. A worker queue is responsible for enqueing [Worker]s, and executing them. See
  * implementors for the implementation details, For example [EventWorkerQueue].
+ *
  * Please note that this interface is likely change on the later phase of refactorings of event queue
  * **/
 internal interface WorkerQueue {
     /**
      * Enqueue a worker that will immediately executes its' work
-     * @param [worker] : [Worker] to be executed
-     * Please note that this behaviour will change after refactorings
+     * @param worker: [Worker] to be executed
+     *
+     * Please note that this behaviour is temporary and will change after refactoring.
      * **/
     fun enqueue(worker: Worker)
 }
