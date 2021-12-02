21729653041e943e49892770a49ad5fbea5d5c5b
ikbalkaya <ceikbal@gmail.com>
2021-12-02 15:06:39 +0000

    Added result handler for ConnectionCreatedWorker

---
+++ publishing-sdk/src/main/java/com/ably/tracking/publisher/workerqueue/resulthandlers/ConnectionCreatedResultHandler.kt
@@ -0,0 +1,44 @@
+package com.ably.tracking.publisher.workerqueue.resulthandlers
+
+import com.ably.tracking.ConnectionException
+import com.ably.tracking.publisher.AddTrackableFailedEvent
+import com.ably.tracking.publisher.ConnectionForTrackableReadyEvent
+import com.ably.tracking.publisher.CorePublisher
+import com.ably.tracking.publisher.TrackableRemovalRequestedEvent
+import com.ably.tracking.publisher.workerqueue.ConnectionCreatedWorkResult
+import com.ably.tracking.publisher.workerqueue.WorkResult
+import com.ably.tracking.publisher.workerqueue.WorkResultHandler
+import com.ably.tracking.publisher.workerqueue.workers.Worker
+
+internal class ConnectionCreatedResultHandler : WorkResultHandler {
+    override fun handle(
+        workResult: WorkResult,
+        corePublisher: CorePublisher
+    ): Worker? {
+        when (workResult) {
+            is ConnectionCreatedWorkResult.RemovalRequested ->
+                corePublisher.request(
+                    TrackableRemovalRequestedEvent(
+                        trackable = workResult.trackable,
+                        handler = workResult.handler,
+                        result = if (workResult.successfulDisconnect) Result.success(Unit) else Result.failure(
+                            workResult.exception
+                                as ConnectionException
+                        )
+                    )
+                )
+
+            is ConnectionCreatedWorkResult.PresenceSuccess -> {
+                corePublisher.request(ConnectionForTrackableReadyEvent(workResult.trackable, workResult.handler))
+            }
+            is ConnectionCreatedWorkResult.PresenceFail -> corePublisher.request(
+                AddTrackableFailedEvent(
+                    workResult.trackable,
+                    workResult.handler,
+                    workResult.exception
+                )
+            )
+        }
+        return null
+    }
+}
