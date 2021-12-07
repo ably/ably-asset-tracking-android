18b25818f7e4710a67ba355f3c5b0a318c817aa4
ikbalkaya <ceikbal@gmail.com>
2021-12-07 17:25:02 +0000

    Update documentation for WorkResultHandler

--- publishing-sdk/src/main/java/com/ably/tracking/publisher/workerqueue/resulthandlers/WorkResultHandler.kt
+++ publishing-sdk/src/main/java/com/ably/tracking/publisher/workerqueue/resulthandlers/WorkResultHandler.kt
@@ -5,16 +5,14 @@ import com.ably.tracking.publisher.workerqueue.results.WorkResult
 import com.ably.tracking.publisher.workerqueue.workers.Worker
 
 /**
- * This interface is intended for handling [WorkResult]s that are received from  [Worker]s synchrnous or asynchronus
+ * This interface is intended for handling [WorkResult]s that are received from  [Worker]s synchronous or asynchronus
  * work.
  *
- * Design note: A more generic interface like WorkResultHandler<T> could have been a nicer choice as implmentors
- * could then expose a narrower typing of [WorkResult]. However I have not found a possible way to represent concrete
- * handlers as generic interface without unchecked casts.
+ * @param T: Type of [WorkResult] to be handled by implementors
  * **/
 internal interface WorkResultHandler<in T : WorkResult> {
     /**
-     * Handles [WorkResult] given to it. Impelementors are responsible for what type of work results they process.
+     * Handles [WorkResult] given to it.
      *
      * @param workResult : [WorkResult] to be handled
      * @param corePublisher: This is a temporary reference of [CorePublisher] that is kept here to maintain
@@ -22,7 +20,7 @@ internal interface WorkResultHandler<in T : WorkResult> {
      *
      * Implementors must delegate work to [corePublisher]  if the required [Worker]s have not been implemented yet.
      *
-     * @return Worker? if implementors decide there is a need to add another worker to the queue.
+     * @return an optional [Worker] if implementors decide there is a need to add another worker to the queue.
      * **/
     fun handle(workResult: T, corePublisher: CorePublisher): Worker?
 }
