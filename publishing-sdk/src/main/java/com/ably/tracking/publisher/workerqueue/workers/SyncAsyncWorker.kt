f20e21d8afed6b2d2dca4010884bbeec0f0422fb
ikbalkaya <ceikbal@gmail.com>
2021-12-07 18:02:47 +0000

    Made SyncAsyncWorker extend new Worker

--- publishing-sdk/src/main/java/com/ably/tracking/publisher/workerqueue/workers/SyncAsyncWorker.kt
+++ publishing-sdk/src/main/java/com/ably/tracking/publisher/workerqueue/workers/SyncAsyncWorker.kt
@@ -8,7 +8,7 @@ import com.ably.tracking.publisher.workerqueue.results.SyncAsyncResult
  * A [SyncAsyncWorker] interface represents workers which executes synchronous work with an optional synchronous work result
  * and another optional asynchronous work.
  * */
-internal interface SyncAsyncWorker {
+internal interface SyncAsyncWorker : Worker<PublisherProperties, SyncAsyncResult> {
     /**
      * A temporary variable that enables sending requests in form of events to CorePublisher, during refactoring
      *
@@ -21,12 +21,12 @@ internal interface SyncAsyncWorker {
      * provide result for the executed synchronous work and an optional asynchronous work the form of a suspending
      * function.
      *
-     * @param properties : Current state of publisher to be used by this worker.
+     * @param input : Current state of publisher to be used by this worker.
      *
-     * Please use [properties] directly only at synchronous part of code. If you need to pass this
+     * Please use [input] directly only at synchronous part of code. If you need to pass this
      * into async work lambda, you should copy necessary properties.
      *
      * @return SyncAsyncResult which represents an optional synchronous work result and an optional asynchronous work
      * **/
-    fun doWork(properties: PublisherProperties): SyncAsyncResult
+    override fun doWork(input: PublisherProperties): SyncAsyncResult
 }
