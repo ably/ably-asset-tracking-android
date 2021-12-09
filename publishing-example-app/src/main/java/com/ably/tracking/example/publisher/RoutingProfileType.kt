394a8f92d97cc5366bfe96916738dd18bcf9d82e
Kacper Kluka <k.kluka@nomtek.com>
2021-12-09 15:36:17 +0100

    Use selected routing profile when creating the publisher service

--- publishing-example-app/src/main/java/com/ably/tracking/example/publisher/RoutingProfileType.kt
+++ publishing-example-app/src/main/java/com/ably/tracking/example/publisher/RoutingProfileType.kt
@@ -1,6 +1,7 @@
 package com.ably.tracking.example.publisher
 
 import androidx.annotation.StringRes
+import com.ably.tracking.publisher.RoutingProfile
 
 enum class RoutingProfileType(@StringRes val displayNameResourceId: Int) {
     DRIVING(R.string.routing_profile_driving),
@@ -8,3 +9,11 @@ enum class RoutingProfileType(@StringRes val displayNameResourceId: Int) {
     WALKING(R.string.routing_profile_walking),
     DRIVING_TRAFFIC(R.string.routing_profile_driving_traffic),
 }
+
+fun RoutingProfileType.toAssetTracking(): RoutingProfile =
+    when (this) {
+        RoutingProfileType.DRIVING -> RoutingProfile.DRIVING
+        RoutingProfileType.CYCLING -> RoutingProfile.CYCLING
+        RoutingProfileType.WALKING -> RoutingProfile.WALKING
+        RoutingProfileType.DRIVING_TRAFFIC -> RoutingProfile.DRIVING_TRAFFIC
+    }
