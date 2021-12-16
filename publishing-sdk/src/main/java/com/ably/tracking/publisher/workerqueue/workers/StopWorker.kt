bfe35ae4d279ed4978244a9ec7e8f0553230b22b
Kacper Kluka <k.kluka@nomtek.com>
2021-12-16 09:42:40 +0100

    Make StopWorker block the queue when it's being processed

--- publishing-sdk/src/main/java/com/ably/tracking/publisher/workerqueue/workers/StopWorker.kt
+++ publishing-sdk/src/main/java/com/ably/tracking/publisher/workerqueue/workers/StopWorker.kt
@@ -9,6 +9,7 @@ import com.ably.tracking.publisher.PublisherProperties
 import com.ably.tracking.publisher.StopEvent
 import com.ably.tracking.publisher.workerqueue.results.StopResult
 import com.ably.tracking.publisher.workerqueue.results.SyncAsyncResult
+import kotlinx.coroutines.runBlocking
 
 internal class StopWorker(
     private val callbackFunction: ResultCallbackFunction<Unit>,
@@ -18,17 +19,20 @@ internal class StopWorker(
     override val event: Event
         get() = StopEvent(callbackFunction)
 
-    override suspend fun doWork(properties: PublisherProperties): SyncAsyncResult {
-        if (properties.isTracking) {
-            corePublisher.stopLocationUpdates(properties)
-        }
-        return try {
-            ably.close(properties.presenceData)
-            properties.dispose()
-            properties.isStopped = true
-            SyncAsyncResult(StopResult.Success(callbackFunction))
-        } catch (exception: ConnectionException) {
-            SyncAsyncResult(StopResult.Fail(callbackFunction, exception))
+    override fun doWork(properties: PublisherProperties): SyncAsyncResult {
+        // We're using [runBlocking] on purpose as we want to block the whole publisher when it's stopping.
+        return runBlocking {
+            if (properties.isTracking) {
+                corePublisher.stopLocationUpdates(properties)
+            }
+            try {
+                ably.close(properties.presenceData)
+                properties.dispose()
+                properties.isStopped = true
+                SyncAsyncResult(StopResult.Success(callbackFunction))
+            } catch (exception: ConnectionException) {
+                SyncAsyncResult(StopResult.Fail(callbackFunction, exception))
+            }
         }
     }
 }
