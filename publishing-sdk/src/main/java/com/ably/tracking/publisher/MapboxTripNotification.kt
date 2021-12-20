2b6f885cc082361dfa8d782e7e614ca9b7eecc51
Kacper Kluka <k.kluka@nomtek.com>
2021-12-20 14:34:03 +0100

    Migrate to Mapbox Navigation 2 in the publisher SDK

--- publishing-sdk/src/main/java/com/ably/tracking/publisher/MapboxTripNotification.kt
+++ publishing-sdk/src/main/java/com/ably/tracking/publisher/MapboxTripNotification.kt
@@ -4,7 +4,7 @@ import android.app.Notification
 import androidx.annotation.Keep
 import com.mapbox.annotation.module.MapboxModule
 import com.mapbox.annotation.module.MapboxModuleType
-import com.mapbox.navigation.base.trip.model.RouteProgress
+import com.mapbox.navigation.base.trip.model.TripNotificationState
 import com.mapbox.navigation.base.trip.notification.TripNotification
 
 @MapboxModule(MapboxModuleType.NavigationTripNotification, enableConfiguration = true)
@@ -17,5 +17,5 @@ internal class MapboxTripNotification(
     override fun getNotificationId(): Int = notificationId
     override fun onTripSessionStarted() = Unit
     override fun onTripSessionStopped() = Unit
-    override fun updateNotification(routeProgress: RouteProgress?) = Unit
+    override fun updateNotification(state: TripNotificationState) = Unit
 }
