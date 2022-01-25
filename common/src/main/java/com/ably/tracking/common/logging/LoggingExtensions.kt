bb52b6757570f68978708f4cbfc113c16e82d1fd
Kacper Kluka <k.kluka@nomtek.com>
2022-01-25 14:24:20 +0100

    Change the format of timestamp in the log messages

--- common/src/main/java/com/ably/tracking/common/logging/LoggingExtensions.kt
+++ common/src/main/java/com/ably/tracking/common/logging/LoggingExtensions.kt
@@ -2,6 +2,9 @@ package com.ably.tracking.common.logging
 
 import com.ably.tracking.logging.LogHandler
 import com.ably.tracking.logging.LogLevel
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 fun LogHandler.v(message: String, throwable: Throwable? = null) {
     log(LogLevel.VERBOSE, message, throwable)
@@ -27,7 +30,9 @@ fun LogHandler.e(throwable: Throwable) {
     log(LogLevel.ERROR, "", throwable)
 }
 
+private val timestampFormat = SimpleDateFormat("dd-MM-yy HH:mm:ss.SSS", Locale.ROOT)
+
 private fun LogHandler.log(level: LogLevel, message: String, throwable: Throwable? = null) {
-    val currentTimestamp = System.currentTimeMillis()
+    val currentTimestamp = timestampFormat.format(Date())
     logMessage(level, "$currentTimestamp: $message", throwable)
 }
