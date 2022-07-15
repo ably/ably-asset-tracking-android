95dc8b75a19570c405abb68c58b1633197c28911
Kacper Kluka <k.kluka@nomtek.com>
2022-07-15 11:17:25 +0200

    Add docs to the KotlinOnly annotation

--- common/src/main/java/com/ably/tracking/common/annotation/KotlinOnly.kt
+++ common/src/main/java/com/ably/tracking/common/annotation/KotlinOnly.kt
@@ -1,3 +1,6 @@
 package com.ably.tracking.common.annotation
 
+/**
+ * An alias for the [JvmSynthetic] annotation that is used in the project to hide annotated methods from the Java API.
+ */
 typealias KotlinOnly = JvmSynthetic
