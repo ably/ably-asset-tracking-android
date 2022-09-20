a8df509765281e39411f9a175e95fe72057263aa
w.dawiskiba <w.dawiskiba@nomtek.com>
2022-09-20 16:14:23 +0200

    AdhocEvent class removed

--- subscribing-sdk/src/main/java/com/ably/tracking/subscriber/SubscriberEvents.kt
+++ subscribing-sdk/src/main/java/com/ably/tracking/subscriber/SubscriberEvents.kt
@@ -8,11 +8,6 @@ import com.ably.tracking.common.ResultCallbackFunction
 internal sealed class Event
 
 /**
- * Represents an event that doesn't have a callback (launch and forget).
- */
-internal sealed class AdhocEvent : Event()
-
-/**
  * Represents an event that invokes an action that calls the [callbackFunction] when it completes.
  */
 internal sealed class Request<T>(val callbackFunction: ResultCallbackFunction<T>) : Event()
