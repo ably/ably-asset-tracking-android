738f227508861009e20f8ae80d982394a162f503
w.dawiskiba <w.dawiskiba@nomtek.com>
2022-09-26 15:42:20 +0200

    wrapInResultCallback utility function added to CallbackFunctionModels.kt

--- common/src/main/java/com/ably/tracking/common/CallbackFunctionModels.kt
+++ common/src/main/java/com/ably/tracking/common/CallbackFunctionModels.kt
@@ -1,5 +1,9 @@
 package com.ably.tracking.common
 
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
 /**
  * Defines a function type, to be implemented in Kotlin code utilising the Ably Asset Tracking SDKs, allowing that code
  * to handle events using that function as a callback (typically using a lamda expression).
@@ -11,3 +15,12 @@ typealias CallbackFunction<T> = (T) -> Unit
  * to handle an event indicating the result of an asynchronous operation.
  */
 typealias ResultCallbackFunction<T> = CallbackFunction<Result<T>>
+
+fun <T> Continuation<T>.wrapInResultCallback(): ResultCallbackFunction<T> =
+    { result ->
+        try {
+            resume(result.getOrThrow())
+        } catch (exception: Exception) {
+            resumeWithException(exception)
+        }
+    }
