f8da7f1b6cffcb4a7497b460dada70fd2f4b8b2e
w.dawiskiba <w.dawiskiba@nomtek.com>
2022-10-24 15:40:09 +0200

    check fixes

--- common/src/main/java/com/ably/tracking/common/Ably.kt
+++ common/src/main/java/com/ably/tracking/common/Ably.kt
@@ -397,7 +397,7 @@ constructor(
     }
 
     private suspend fun failChannel(channel: Channel, presenceData: PresenceData, errorInfo: ErrorInfo) {
-        //Once a channel enters failed state we can reattach it again, so we're not removing the reference
+        // Once a channel enters failed state we can reattach it again, so we're not removing the reference
         channel.setConnectionFailed(errorInfo)
         leavePresence(channel, presenceData)
         channel.unsubscribe()
