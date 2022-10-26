a3f312a0d4ba9e6d4e4d20955709cbdd34e7f80a
w.dawiskiba <w.dawiskiba@nomtek.com>
2022-10-26 14:07:16 +0200

    copying PublisherProperties implemented

--- publishing-sdk/src/main/java/com/ably/tracking/publisher/PublisherProperties.kt
+++ publishing-sdk/src/main/java/com/ably/tracking/publisher/PublisherProperties.kt
@@ -16,14 +16,40 @@ import com.ably.tracking.publisher.guards.DuplicateTrackableGuard
 import com.ably.tracking.publisher.guards.TrackableRemovalGuard
 import kotlinx.coroutines.flow.MutableStateFlow
 
-internal class PublisherProperties(
+internal class PublisherProperties
+private constructor(
     routingProfile: RoutingProfile,
     locationEngineResolution: Resolution,
     isLocationEngineResolutionConstant: Boolean,
     areRawLocationsEnabled: Boolean?,
+    enhancedLocationsPublishingState: LocationsPublishingState<EnhancedLocationUpdate>,
+    rawLocationsPublishingState: LocationsPublishingState<LocationUpdate>,
+    duplicateTrackableGuard: DuplicateTrackableGuard,
+    trackableRemovalGuard: TrackableRemovalGuard,
     private val onActiveTrackableUpdated: (Trackable?) -> Unit,
     private val onRoutingProfileUpdated: (RoutingProfile) -> Unit
 ) : Properties() {
+
+    constructor(
+        routingProfile: RoutingProfile,
+        locationEngineResolution: Resolution,
+        isLocationEngineResolutionConstant: Boolean,
+        areRawLocationsEnabled: Boolean?,
+        onActiveTrackableUpdated: (Trackable?) -> Unit,
+        onRoutingProfileUpdated: (RoutingProfile) -> Unit
+    ) : this(
+        routingProfile,
+        locationEngineResolution,
+        isLocationEngineResolutionConstant,
+        areRawLocationsEnabled,
+        LocationsPublishingState(),
+        LocationsPublishingState(),
+        DefaultDuplicateTrackableGuard(),
+        DefaultTrackableRemovalGuard(),
+        onActiveTrackableUpdated,
+        onRoutingProfileUpdated
+    )
+
     private var isDisposed: Boolean = false
     var locationEngineResolution: Resolution = locationEngineResolution
         get() = if (isDisposed) throw PublisherPropertiesDisposedException() else field
@@ -83,14 +109,13 @@ internal class PublisherProperties(
     val rawLocationChangedCommands: MutableList<(PublisherProperties) -> Unit> = mutableListOf()
         get() = if (isDisposed) throw PublisherPropertiesDisposedException() else field
     val enhancedLocationsPublishingState: LocationsPublishingState<EnhancedLocationUpdate> =
-        LocationsPublishingState()
+        enhancedLocationsPublishingState
         get() = if (isDisposed) throw PublisherPropertiesDisposedException() else field
-    val rawLocationsPublishingState: LocationsPublishingState<LocationUpdate> =
-        LocationsPublishingState()
+    val rawLocationsPublishingState: LocationsPublishingState<LocationUpdate> = rawLocationsPublishingState
         get() = if (isDisposed) throw PublisherPropertiesDisposedException() else field
-    val duplicateTrackableGuard: DuplicateTrackableGuard = DefaultDuplicateTrackableGuard()
+    val duplicateTrackableGuard: DuplicateTrackableGuard = duplicateTrackableGuard
         get() = if (isDisposed) throw PublisherPropertiesDisposedException() else field
-    val trackableRemovalGuard: TrackableRemovalGuard = DefaultTrackableRemovalGuard()
+    val trackableRemovalGuard: TrackableRemovalGuard = trackableRemovalGuard
         get() = if (isDisposed) throw PublisherPropertiesDisposedException() else field
     val areRawLocationsEnabled: Boolean = areRawLocationsEnabled ?: false
     var state: PublisherState = PublisherState.IDLE
@@ -107,9 +132,43 @@ internal class PublisherProperties(
     override val isStopped: Boolean
         get() = state == PublisherState.STOPPED
 
-    fun copy(): PublisherProperties {
-        throw NotImplementedError()
-    }
+    fun copy(): PublisherProperties =
+        PublisherProperties(
+            routingProfile,
+            locationEngineResolution,
+            isLocationEngineResolutionConstant,
+            areRawLocationsEnabled,
+            enhancedLocationsPublishingState,
+            rawLocationsPublishingState,
+            duplicateTrackableGuard,
+            trackableRemovalGuard,
+            onActiveTrackableUpdated,
+            onRoutingProfileUpdated
+        )
+            .also {
+                it.isDisposed = isDisposed
+                it.isTracking = isTracking
+                it.trackables.addAll(trackables)
+                it.trackableStates.putAll(trackableStates)
+                it.trackableSubscribedToPresenceFlags.putAll(trackableSubscribedToPresenceFlags)
+                it.trackableStateFlows.putAll(trackableStateFlows)
+                it.lastChannelConnectionStateChanges.putAll(lastChannelConnectionStateChanges)
+                it.lastConnectionStateChange = lastConnectionStateChange
+                it.resolutions.putAll(resolutions)
+                it.lastSentEnhancedLocations.putAll(lastSentEnhancedLocations)
+                it.lastSentRawLocations.putAll(lastSentRawLocations)
+                it.skippedEnhancedLocations.addAll(skippedEnhancedLocations)
+                it.skippedRawLocations.addAll(skippedEnhancedLocations)
+                it.estimatedArrivalTimeInMilliseconds = estimatedArrivalTimeInMilliseconds
+                it.lastPublisherLocation = lastPublisherLocation
+                it.currentDestination = currentDestination
+                it.subscribers.putAll(subscribers)
+                it.requests.putAll(requests)
+                it.presenceData = presenceData
+                it.active = active
+                it.rawLocationChangedCommands.addAll(rawLocationChangedCommands)
+                it.state = state
+            }
 
     fun dispose() {
         trackables.clear()
