aa8808481405f5241bb5528a59b4de1246e3e6ff
Kacper Kluka <k.kluka@nomtek.com>
2022-11-30 13:23:51 +0100

    Split callback and static authentication in the AblyHelpers file

--- common/src/main/java/com/ably/tracking/common/AblyHelpers.kt
+++ common/src/main/java/com/ably/tracking/common/AblyHelpers.kt
@@ -88,40 +88,36 @@ val Authentication.clientOptions: ClientOptions
     get() = ClientOptions().apply {
         clientId = this@clientOptions.clientId
 
-        this@clientOptions.tokenRequestConfiguration?.let { tokenRequestConfiguration ->
-            tokenRequestConfiguration.callback?.let { tokenRequestCallback ->
-                authCallback = Auth.TokenCallback {
-                    runBlocking {
-                        try {
-                            tokenRequestCallback(it.toTracking()).toAuth()
-                        } catch (exception: TokenAuthException) {
-                            throw exception.toAblyException()
-                        }
+        this@clientOptions.tokenRequestConfiguration?.callback?.let { tokenRequestCallback ->
+            authCallback = Auth.TokenCallback {
+                runBlocking {
+                    try {
+                        tokenRequestCallback(it.toTracking()).toAuth()
+                    } catch (exception: TokenAuthException) {
+                        throw exception.toAblyException()
                     }
                 }
-            }
-
-            tokenRequestConfiguration.staticTokenRequest?.let { staticTokenRequest ->
-                authCallback = Auth.TokenCallback { staticTokenRequest.toAuth() }
             }
         }
 
-        this@clientOptions.jwtConfiguration?.let { jwtConfiguration ->
-            jwtConfiguration.callback?.let { jwtCallback ->
-                authCallback = Auth.TokenCallback {
-                    runBlocking {
-                        try {
-                            jwtCallback(it.toTracking())
-                        } catch (exception: TokenAuthException) {
-                            throw exception.toAblyException()
-                        }
+        this@clientOptions.tokenRequestConfiguration?.staticTokenRequest?.let { staticTokenRequest ->
+            authCallback = Auth.TokenCallback { staticTokenRequest.toAuth() }
+        }
+
+        this@clientOptions.jwtConfiguration?.callback?.let { jwtCallback ->
+            authCallback = Auth.TokenCallback {
+                runBlocking {
+                    try {
+                        jwtCallback(it.toTracking())
+                    } catch (exception: TokenAuthException) {
+                        throw exception.toAblyException()
                     }
                 }
             }
+        }
 
-            jwtConfiguration.staticJwt?.let { staticJwt ->
-                authCallback = Auth.TokenCallback { staticJwt }
-            }
+        this@clientOptions.jwtConfiguration?.staticJwt?.let { staticJwt ->
+            authCallback = Auth.TokenCallback { staticJwt }
         }
 
         this@clientOptions.basicApiKey?.let { basicApiKey ->
