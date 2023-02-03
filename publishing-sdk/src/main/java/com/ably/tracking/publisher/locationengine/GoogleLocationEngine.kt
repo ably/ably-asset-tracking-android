cdc889160e2ebf06b41ed0fbb110dcc0e70b12b2
w.dawiskiba <w.dawiskiba@nomtek.com>
2023-02-03 15:58:34 +0100

    pending intent null check added back to GoogleLocationEngine.kt

--- publishing-sdk/src/main/java/com/ably/tracking/publisher/locationengine/GoogleLocationEngine.kt
+++ publishing-sdk/src/main/java/com/ably/tracking/publisher/locationengine/GoogleLocationEngine.kt
@@ -48,7 +48,12 @@ class GoogleLocationEngine(context: Context) : ResolutionLocationEngine {
 
     @SuppressLint("MissingPermission")
     override fun requestLocationUpdates(request: LocationEngineRequest, pendingIntent: PendingIntent?) {
-        fusedLocationProviderClient.requestLocationUpdates(toGMSLocationRequest(request), pendingIntent)
+        if (pendingIntent != null) {
+            fusedLocationProviderClient.requestLocationUpdates(
+                toGMSLocationRequest(request),
+                pendingIntent
+            )
+        }
     }
 
     override fun removeLocationUpdates(callback: LocationEngineCallback<LocationEngineResult>) {
