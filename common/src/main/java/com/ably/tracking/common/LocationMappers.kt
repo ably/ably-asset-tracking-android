e6ac95754a1d8ec32250f9c5b23679b6837adc8b
w.dawiskiba <w.dawiskiba@nomtek.com>
2023-02-20 19:00:26 +0100

    Location parts checks rationale comment added

--- common/src/main/java/com/ably/tracking/common/LocationMappers.kt
+++ common/src/main/java/com/ably/tracking/common/LocationMappers.kt
@@ -3,6 +3,8 @@ package com.ably.tracking.common
 import com.ably.tracking.Location
 
 fun android.location.Location.toAssetTracking(timestamp: Long = time): Result<Location> {
+    // According to Location class documentation, i.e. https://developer.android.com/reference/android/location/Location#getAltitude(),
+    // those fields have a real value only if “has function” returns true. Those checks prevent passing bogus data.
     val altitude = if (hasAltitude()) {
         altitude
     } else {
